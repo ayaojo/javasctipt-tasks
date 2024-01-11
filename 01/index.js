cdf1d3b9e38d4b3efa0f700a2c9b7ca19f3c4f6b
ayaojo <ojokuki01@gmail.com>
2024-01-11 23:16:14 +0600

    add 1st task

---
+++ 01/index.js
@@ -0,0 +1,32 @@
+// task 1
+const userName = prompt("Your name:")
+console.log(alert(`Hello, ${userName}!`))
+
+// task 2
+
+
+// task 3
+
+
+// task 4
+
+
+// task 5
+
+
+// task 6
+
+
+// task 7
+
+
+// task 8
+
+
+// task 9
+
+
+// task 10
+
+
+// task 11
