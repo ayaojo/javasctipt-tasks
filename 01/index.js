c090c29c362171f0458e5d74e9c0619c6b0c6163
ayaojo <ojokuki01@gmail.com>
2024-01-12 13:47:27 +0600

    add all tasks

--- 01/index.js
+++ 01/index.js
@@ -3,32 +3,96 @@
 console.log(alert(`Hello, ${userName}!`)) */
 
 // task 2
-const num = prompt("your number:")
+/* const num = prompt("your number:")
 const extent = prompt("degree of the number:") 
-console.log(num**extent)
-
-// task 3
-
+console.log(num**extent) */
 
 // task 4
-
+/* const text = "my text"
+if (text == "some text") {
+    console.log("another text")
+} else {
+    console.log("some text")
+} */
 
 // task 5
-
+/* let num = 3
+if (num == 0) {
+    console.log(1)
+} else if (num < 0) {
+    console.log("less then zero")
+} else {
+    console.log(num *= 10)
+} */
 
 // task 6
-
+/* const num = Number(prompt("your number:"))
+let result
+if (!isNaN(num)) {
+    if (num < 5) {
+        console.log(result = 0)
+    } else {
+        console.log(result = 1)
+    } 
+} else {
+    console.log("enter number");
+} */
 
 // task 7
-
+/* const num1 = Number(prompt("your first number:"))
+const num2 = Number(prompt("your second number:"))
+if (num1 > num2) {
+    console.log(`Biggiest number: ${num1}`)
+} else if (num1 === num2) {
+    console.log(`They are same`)
+} else if (num1 < num2) {
+    console.log(`Biggiest number: ${num2}`)
+}
+ else {
+    console.log("enter number");
+} */
 
 // task 8
-
+/* const num1 = prompt("first number:")
+const num2 = prompt("second number:")
+if (num1 % num2 === 0){
+    console.log(`${num1} multiple ${num2}`)
+} else {
+    console.log(`${num1} not multiple ${num2}`)
+} */
 
 // task 9
-
+/* const averageScore = Number(prompt("your GPA:"))
+if (averageScore > 1 && averageScore < 4){
+    console.log("You stupid, sorry. it's not my fault")
+} else if (averageScore > 5 && averageScore < 8){
+    console.log("ok, but don't be lazzy ")
+} else if(averageScore > 9 && averageScore < 10){
+    console.log("Great job!")
+} else{
+    console.log("write your GPA!!!")
+} */
 
 // task 10
-
+/* const examScore = prompt("your score for of exam:")
+const projectNum = prompt("number of completed projects:")
+if (examScore > 90 && projectNum > 10){
+    console.log("your graduation score - 100")
+} else if (examScore > 75 && projectNum > 5){
+    console.log("your graduation score - 90")
+}else if (examScore > 50 && projectNum > 2){
+    console.log("your graduation score - 75")
+} else {
+    console.log("your graduation score - 0")
+} */
 
 // task 11
+/* const rentCost = 40
+const rentDays = prompt("the number of days for which you need to rent a car")
+if (rentDays == 1) {
+    console.log(`car rent costs $${rentCost}`)
+} else if (rentDays >= 7) {
+    console.log(`car rent costs $${rentCost - 50}`)
+} else {
+    console.log(`car rent costs $${rentCost - 20}`)
+} */
